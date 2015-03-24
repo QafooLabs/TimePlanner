a2b6de29b6779e33f6d1fb368b5d74bedbf5d88d
Kore Nordmann <mail@kore-nordmann.de>
2015-03-24 15:27:14 +0100

    Implemented static file handling for PHP internals web server

--- web/index.php
+++ web/index.php
@@ -5,6 +5,13 @@ require_once __DIR__ . "/../app/AppKernel.php";
 
 use Symfony\Component\HttpFoundation\Request;
 
+if (file_exists(__DIR__ . $_SERVER['REQUEST_URI']) &&
+    is_file(__DIR__ . $_SERVER['REQUEST_URI'])) {
+    // Do not try to server static files – this is only important, if used
+    // together with PHPs internal webserver.
+    return false;
+}
+
 $request = Request::createFromGlobals();
 $kernel = new AppKernel(AppKernel::getEnvironmentFromConfiguration(), AppKernel::getDebug());
 $response = $kernel->handle($request);

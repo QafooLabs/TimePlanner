85021c9b1faa6614f57f0275af9d6438d370328a
Kore Nordmann <mail@kore-nordmann.de>
2015-03-26 10:50:09 +0100

    Implemented basic user gateway / provider

---
+++ src/Qafoo/UserBundle/Gateway/User.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Qafoo\UserBundle\Gateway;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Doctrine\ODM\CouchDB\DocumentManager;
+
+class User implements UserProviderInterface
+{
+    const USER_CLASS = '\\Qafoo\\UserBundle\\Domain\\FOSUserHelper';
+
+    /**
+     * Document manager
+     *
+     * @var DocumentManager
+     */
+    private $documentManager;
+
+    public function __construct(DocumentManager $documentManager)
+    {
+        $this->documentManager = $documentManager;
+
+    }
+
+    /**
+     * Loads the user for the given username.
+     *
+     * This method must throw UsernameNotFoundException if the user is not
+     * found.
+     *
+     * @param string $username The username
+     *
+     * @return UserInterface
+     *
+     * @see UsernameNotFoundException
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     */
+    public function loadUserByUsername($username)
+    {
+        $user = $this->documentManager->find(self::USER_CLASS, $username);
+
+        if (!$user) {
+            throw new UsernameNotFoundException(sprintf('Username "%s" does not exist.', $username));
+        }
+
+        return $user;
+    }
+
+    /**
+     * Refreshes the user for the account interface.
+     *
+     * It is up to the implementation to decide if the user data should be
+     * totally reloaded (e.g. from the database), or if the UserInterface
+     * object can just be merged into some internal array of users / identity
+     * map.
+     *
+     * @param UserInterface $user
+     *
+     * @return UserInterface
+     *
+     * @throws UnsupportedUserException if the account is not supported
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        return $user;
+    }
+
+    /**
+     * Whether this provider supports the given user class.
+     *
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function supportsClass($class)
+    {
+        return self::USER_CLASS === $class ||
+            is_subclass_of($class, self::USER_CLASS);
+    }
+}

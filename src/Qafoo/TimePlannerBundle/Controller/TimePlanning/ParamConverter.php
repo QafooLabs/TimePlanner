aa4153ddcd6a88998d4fa8b1e760447d7d994bd6
Kore Nordmann <mail@kore-nordmann.de>
2015-04-02 16:39:40 +0200

    Added param converter for jobs

---
+++ src/Qafoo/TimePlannerBundle/Controller/TimePlanning/ParamConverter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Qafoo\TimePlannerBundle\Controller\TimePlanning;
+
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter as Configuration;
+use Symfony\Component\HttpFoundation\Request;
+
+use Qafoo\TimePlannerBundle\Gateway\JobGateway;
+
+class ParamConverter implements ParamConverterInterface
+{
+    /**
+     * Gateway
+     *
+     * @var JobGateway
+     */
+    private $gateway;
+
+    public function __construct(JobGateway $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
+    public function apply(Request $request, Configuration $configuration)
+    {
+        $jobId = $request->attributes->get('job');
+        if (!$jobId) {
+            return false;
+        }
+
+        $job = $this->gateway->get($jobId);
+
+        $parameterName = $configuration->getName();
+        $request->attributes->set($parameterName, $job);
+
+        return true;
+    }
+
+    public function supports(Configuration $configuration)
+    {
+        return "Qafoo\\TimePlannerBundle\\Domain\\Job" === $configuration->getClass();
+    }
+}

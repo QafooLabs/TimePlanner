51cd3d3e589a242b897c109d6d91a361ec889694
Kore Nordmann <mail@kore-nordmann.de>
2015-04-02 12:22:02 +0200

    Implemented monthly available workdays calculation

--- src/Qafoo/TimePlannerBundle/Domain/JobService.php
+++ src/Qafoo/TimePlannerBundle/Domain/JobService.php
@@ -2,8 +2,38 @@
 
 namespace Qafoo\TimePlannerBundle\Domain;
 
+use Qafoo\TimePlannerBundle\Gateway\JobGateway;
+
 class JobService
 {
+    /**
+     * Job gateway
+     *
+     * @var JobGateway
+     */
+    private $jobGateway;
+
+    /**
+     * Public holiday service
+     *
+     * @var PublicHolidayService
+     */
+    private $publicHolidayService;
+
+    /**
+     * Expected sick leave [0 … 1]
+     *
+     * @var float
+     */
+    private $expectedSickLeave;
+
+    public function __construct(JobGateway $jobGateway, PublicHolidayService $publicHolidayService, $expectedSickLeave)
+    {
+        $this->jobGateway = $jobGateway;
+        $this->publicHolidayService = $publicHolidayService;
+        $this->expectedSickLeave = $expectedSickLeave;
+    }
+
     /**
      * Get jobs
      *
@@ -14,5 +44,37 @@ class JobService
     public function getJobs($year, $month)
     {
         return array();
+    }
+
+    /**
+     * Get work days
+     *
+     * @param int $year
+     * @param int $month
+     * @return DaySet
+     */
+    public function getWorkDays($year, $month)
+    {
+        $workDays = DaySet::createFromRange(
+            $start = new Day("1.$month.$year"),
+            $start->modify("last day of this month")
+        )->filter(
+            function (Day $day) {
+                return !$day->isWeekend();
+            }
+        );
+        return $workDays->diff($this->publicHolidayService->getHolidayDays($year));
+    }
+
+    /**
+     * Get available work days
+     *
+     * @param int $year
+     * @param int $month
+     * @return int
+     */
+    public function getAvailableWorkDays($year, $month)
+    {
+        return floor(count($this->getWorkDays($year, $month)) * (1 - $this->expectedSickLeave));
     }
 }

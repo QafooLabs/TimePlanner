99c3e4b44049133a1276241bf5d420cda5c3b527
Kore Nordmann <mail@kore-nordmann.de>
2015-04-02 11:04:20 +0200

    Added Job domain object

---
+++ src/Qafoo/TimePlannerBundle/Domain/Job.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Qafoo\TimePlannerBundle\Domain;
+
+use Kore\DataObject\DataObject;
+
+class Job extends DataObject
+{
+    /**
+     * Job ID
+     *
+     * @var string
+     */
+    public $jobId;
+
+    /**
+     * Customer
+     *
+     * @var string
+     */
+    public $customer;
+
+    /**
+     * Project
+     *
+     * @var string
+     */
+    public $project;
+
+    /**
+     * Person days
+     *
+     * @var PersonDays
+     */
+    public $personDays;
+
+    /**
+     * Expected revenue
+     *
+     * May be a number or a formular based om minimumPersonDays
+     *
+     * @var strin
+     */
+    public $expectedRevenue;
+
+    /**
+     * Assignees
+     *
+     * @var Job\Assignment[]
+     */
+    public $assignees = array();
+
+    /**
+     * Invoice ID
+     *
+     * @var string
+     */
+    public $invoiceId;
+
+    /**
+     * Comment
+     *
+     * @var string
+     */
+    public $comment;
+}

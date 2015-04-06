4e810a19a2ecae43ec2326df810ba1cdd256e517
Kore Nordmann <mail@kore-nordmann.de>
2015-04-06 20:59:19 +0200

    Implemented job editing

---
+++ src/Qafoo/TimePlannerBundle/Controller/TimePlanning/Edit.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Qafoo\TimePlannerBundle\Controller\TimePlanning;
+
+use Kore\DataObject\DataObject;
+
+class Edit extends DataObject
+{
+    /**
+     * Job
+     *
+     * @var Job
+     */
+    public $job;
+}

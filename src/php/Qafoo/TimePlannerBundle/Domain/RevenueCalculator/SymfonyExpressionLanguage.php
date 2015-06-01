25047c9dffb24fbb7bcfa558878e450ffa909ffe
Kore Nordmann <mail@kore-nordmann.de>
2015-06-01 09:49:53 +0200

    Convert array collection

--- src/php/Qafoo/TimePlannerBundle/Domain/RevenueCalculator/SymfonyExpressionLanguage.php
+++ src/php/Qafoo/TimePlannerBundle/Domain/RevenueCalculator/SymfonyExpressionLanguage.php
@@ -62,7 +62,7 @@ class SymfonyExpressionLanguage extends RevenueCalculator
                             function (Job\Assignment $assignment) {
                                 return $assignment->days;
                             },
-                            $job->assignees ?: array()
+                            is_object($job->assignees) ? $job->assignees->toArray() : $job->assignees
                         )
                     )
                 )
